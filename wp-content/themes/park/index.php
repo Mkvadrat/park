0582cee5b7b5a29e0c55597fc357ae309a1839ab
Mathews Wednesday <q3dm0@yahoo.com>
2019-05-22 17:21:02 +0300

    Услуги, акции

--- wp-content/themes/park/index.php
+++ wp-content/themes/park/index.php
@@ -10,8 +10,28 @@ Version: 1.0
 
 get_header(); ?>
 	
-	<?php if (have_posts()): while (have_posts()): the_post(); ?>
-		<?php the_content(); ?>
-	<?php endwhile; endif; ?>
+    <div id="content" class="main">
+        <div class="wrapper">
+
+            <div class="banner__top">
+                <?php
+                    $image_url = wp_get_attachment_image_src( get_post_thumbnail_id(get_the_ID()), 'full'); 
+                ?>
+                <div class="bg__slider" data-parallax="scroll" data-image-src="<?php echo $image_url[0] ? $image_url[0] : esc_url( get_template_directory_uri() ) . '/img/massandra.jpg' ?>">
+                    <div class="slider__description">
+                        <div class="slider__text"><h3><?php the_title(); ?></h3></div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="section pb10">
+                <div class="max__wrap-p">
+                    <?php if (have_posts()): while (have_posts()): the_post(); ?>
+                        <?php the_content(); ?>
+                    <?php endwhile; endif; ?>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php get_footer(); ?>
